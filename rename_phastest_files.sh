7033cf5411bf759d9aa23d51614d78cd5369d882
pjoglek <pjoglek@ncsu.edu>
2024-09-17 09:47:51 -0400

    Renames phastests region_DNA.txt file with directory (bacterial accession) basename

---
+++ rename_phastest_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p renamed_phasted_files
+
+for dir in /Users/pjoglekar/work/software/shared_docker_image/phastest-docker/phastest-app-docker/JOBS/*; do
+    if [ -d "$dir" ]; then
+        base=$(basename "$dir" | sed 's/_renamed//')
+        src_file="$dir/region_DNA.txt"
+        dest_file="renamed_phasted_files/${base}_region_DNA.txt"
+        
+        if [ -f "$src_file" ]; then
+            cp "$src_file" "$dest_file"
+            echo "Copied $src_file to $dest_file"
+        else
+            echo "File $src_file does not exist"
+        fi
+    fi
+done
