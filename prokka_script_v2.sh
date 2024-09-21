dadf36e8ef4237338d05a61069e5cf068714b4db
pjoglek <pjoglek@ncsu.edu>
2024-09-20 22:19:38 -0400

    Prokka script changed to  get input output directories from command line

---
+++ prokka_script_v2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source /Users/pjoglekar/miniforge3/etc/profile.d/conda.sh # Load conda
+
+conda activate prokka_env
+
+# Define the directory containing your FASTA files
+fasta_dir=$1
+output_dir=$2
+
+# Loop through each FASTA file in the directory
+for fasta_file in "$fasta_dir"/*.fasta
+do
+  # Get the base name of the file (without the directory and extension)
+  base_name=$(basename "$fasta_file" .fasta)
+  
+  # Run Prokka annotation
+  prokka --outdir "$output_dir/$base_name" --prefix "$base_name" "$fasta_file"
+
+  mkdir -p "$output_dir"/prokka_gff_files
+
+  for f in "$output_dir/$base_name"/*.gff; do
+    mv "$f" "$output_dir"/prokka_gff_files
+  done  
+done
+
+conda deactivate
